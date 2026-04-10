0b5e6ac17b0833d6b410d54fdf29972c8b95ec3f
Raython <rasheed998855@gmail.com>
2026-04-10 17:24:33 +0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -8,12 +8,13 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 
-// AcceptSocket() blocks here until a client connects, then returns the connected socket
-Socket client = server.AcceptSocket();
+while(true){
+    Socket client = server.AcceptSocket();
 
-// Read what the client sent (e.g. the PING command)
-byte[] buffer = new byte[1024];
-client.Receive(buffer);
+    // Read what the client sent (e.g. the PING command)
+    byte[] buffer = new byte[1024];
+    client.Receive(buffer);
 
-// Send back the PONG response
-client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+    // Send back the PONG response
+    client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+}
