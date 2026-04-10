b8fd4b168cdd8bd9e35454a423d65319f7ca29a8
Raython <rasheed998855@gmail.com>
2026-04-10 16:58:18 +0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,19 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment the code below to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+// AcceptSocket() blocks here until a client connects, then returns the connected socket
+Socket client = server.AcceptSocket();
+
+// Read what the client sent (e.g. the PING command)
+byte[] buffer = new byte[1024];
+client.Receive(buffer);
+
+// Send back the PONG response
+client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
